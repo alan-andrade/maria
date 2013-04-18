2e44cf9a55447d137c65ae718e83be043f2d228a
Alan Andrade <alan.andradec@gmail.com>
2013-04-18 10:26:13 -0700

    override remote origin

--- lib/maria/engine.rb
+++ lib/maria/engine.rb
@@ -14,13 +14,18 @@ module Maria
 
     Git.root = Maria::Engine.root
 
+    # Keep and eye on this line, it might be better to store them somwhere else.
+    ::FileControl.root_path = Engine.root + 'app/views'
+
     if Rails.env.test? or Rails.env.development?
       require 'git/test'
-      # Keep and eye on this line, it might be better to store them somwhere else.
-      ::FileControl.root_path = Engine.root + 'app/views'
       Git::Repo.tear_fake_remote
       Git::Repo.set_fake_remote
       Git::Test.set_fake_remote_branches
+    else
+      Git::Repo.set_real_remote
+      Git.remote = 'origin'
+      Git.remote_url = "remotes/origin/#{Git::Branch.current}"
     end
 
   end
